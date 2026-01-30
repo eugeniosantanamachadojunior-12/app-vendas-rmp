add08d52787f6b7b4a5f682fbc7223367b37b128
Eugenio Santana Machado <eugeniosantanamachadojunior@gmail.com>
2026-01-30 12:08:17 -0300

    migrate: mysql -> postgres

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,12 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
