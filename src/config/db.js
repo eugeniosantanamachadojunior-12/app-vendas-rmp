86ef08855667dc15c691c61fe9c2560d4305fdc5
Eugenio Santana Machado <eugeniosantanamachadojunior@gmail.com>
2026-01-30 16:25:05 -0300

    Configuração de SSL para o banco Aiven

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,18 @@
-const { Pool } = require("pg");
+const mysql = require("mysql2/promise");
 
-const pool = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-  ssl: { rejectUnauthorized: false }
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  // ADICIONE ESTA PARTE PARA O AIVEN:
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
