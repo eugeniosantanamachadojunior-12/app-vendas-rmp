a7f1e5399566fd8a573a71214a36362b0da6eee6
Eugenio Santana Machado <eugeniosantanamachadojunior@gmail.com>
2026-02-02 15:57:56 -0300

    Debug env vars

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql2/promise");
 
+// DEBUG (temporário)
+console.log("HOST:", process.env.DB_HOST);
+console.log("USER:", process.env.DB_USER);
+console.log("PASS:", process.env.DB_PASSWORD);
+console.log("NAME:", process.env.DB_NAME);
+console.log("PORT:", process.env.DB_PORT);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
