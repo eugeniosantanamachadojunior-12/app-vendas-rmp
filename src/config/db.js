50ad1bca7c028e1b2a95892575c5636d1ddc6fcb
Eugenio Santana Machado <eugeniosantanamachadojunior@gmail.com>
2026-02-02 16:03:34 -0300

    Remove debug logs

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,5 @@
 const mysql = require("mysql2/promise");
 
-// DEBUG (temporário)
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("PASS:", process.env.DB_PASSWORD);
-console.log("NAME:", process.env.DB_NAME);
-console.log("PORT:", process.env.DB_PORT);
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
