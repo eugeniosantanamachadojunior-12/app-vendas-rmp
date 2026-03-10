06edd85d2e2f26b307f8812202f8d9d7cff0ff12
Eugenio Santana Machado <eugeniosantanamachadojunior@gmail.com>
2026-03-10 17:49:52 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -6,7 +6,10 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }
+  // ESTA É A CHAVE PARA O SUCESSO:
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
